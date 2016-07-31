947cb437787b1e5145a2653ad792e27beb49202b
Bradley Shaw <admin@indigotiger.me>
2016-08-01 00:01:34 +0100

    Add ECDSA-NIST256P-CHALLENGE SASL method

--- modules/sasl.cpp
+++ modules/sasl.cpp
@@ -18,6 +18,14 @@
 #include <znc/IRCSock.h>
 #include <algorithm>
 
+#ifdef HAVE_LIBSSL
+#include <openssl/ec.h>
+#include <openssl/ecdsa.h>
+#include <openssl/evp.h>
+#include <openssl/rand.h>
+#include <openssl/sha.h>
+#endif
+
 static const struct {
     const char* szName;
     const char* szDescription;
@@ -26,8 +34,130 @@ static const struct {
       {"EXTERNAL", "TLS certificate, for use with the *cert module", true},
       {"PLAIN",
        "Plain text negotiation, this should work always if the network "
-       "supports SASL",
-       true}};
+       "supports SASL", true},
+#ifdef HAVE_LIBSSL
+      {"ECDSA-NIST256P-CHALLENGE", "ECDSA public key authentication", true},
+#endif
+};
+
+#ifdef HAVE_LIBSSL
+class ECDACommon {
+private:
+    EC_KEY *key;
+    bool loaded;
+
+public:
+    ECDACommon() {
+        key = EC_KEY_new_by_curve_name(NID_X9_62_prime256v1);
+        EC_KEY_set_conv_form(key, POINT_CONVERSION_COMPRESSED);
+        loaded = false;
+    }
+
+    ~ECDACommon() {
+        if (key)
+            EC_KEY_free(key);
+    }
+
+    /* Generate an ECC keypair suitable for authentication. */
+    void GenerateKey(void) {
+        EC_KEY_generate_key(key);
+        loaded = true;
+    }
+
+    /* Get base64 encoded private key from key */
+    CString GetPubBase64(void) {
+        CString ret;
+        size_t keybuflen;
+        unsigned char *keybuf;
+
+        if (!loaded)
+            return ret;
+
+        keybuflen = (size_t) i2o_ECPublicKey(key, NULL);
+        keybuf = (unsigned char *)malloc(keybuflen);
+        if (!keybuf)
+            return ret;
+
+        if (!i2o_ECPublicKey(key, &keybuf)) {
+            free(keybuf);
+            return ret;
+        }
+        /* for some reason keybuf is at the end now ?! */
+        keybuf -= keybuflen;
+
+        ret = CString((char *)keybuf, keybuflen);
+        ret.Base64Encode();
+
+        free(keybuf);
+        return ret;
+    }
+
+    /* returns false if key fails to parse */
+    bool LoadKey(const CString& sPath) {
+        FILE *keyfile;
+        CString path = sPath + "/sasl.pem";
+        keyfile = fopen(path.c_str(), "r");
+        if (keyfile == NULL) {
+            loaded = false;
+            return loaded;
+        }
+
+        PEM_read_ECPrivateKey(keyfile, &key, NULL, NULL);
+        fclose(keyfile);
+
+        EC_KEY_set_conv_form(key, POINT_CONVERSION_COMPRESSED);
+
+        if (!EC_KEY_check_key(key))
+            loaded = false;
+        else
+            loaded = true;
+
+        return loaded;
+    }
+
+    bool SaveKey(const CString& sPath) {
+        FILE *keyfile;
+        CString path = sPath + "/sasl.pem";
+        if (!loaded)
+            return loaded;
+        keyfile = fopen(path.c_str(), "w");
+        if (keyfile == NULL)
+            return false;
+        PEM_write_ECPrivateKey(keyfile, key, NULL, NULL, 0, NULL, NULL);
+        fclose(keyfile);
+        return true;
+    }
+
+    CString Sign(const CString& challenge) {
+        CString ret;
+        CString ch;
+        unsigned char *sigbuf;
+        unsigned int siglen;
+
+        if (!loaded)
+            return ret;
+
+        siglen = ECDSA_size(key);
+        sigbuf = (unsigned char *)malloc(siglen);
+        if (!sigbuf)
+            return ret;
+
+        ch = challenge.Base64Decode_n();
+
+        if (!ECDSA_sign(0, (unsigned char *)ch.c_str(), ch.size(), sigbuf, &siglen, key)) {
+            free(sigbuf);
+            return ret;
+        }
+
+        ret = CString((char *)sigbuf, siglen);
+        ret.Base64Encode();
+        free(sigbuf);
+
+        return ret;
+    }
+
+};
+#endif
 
 #define NV_REQUIRE_AUTH "require_auth"
 #define NV_MECHANISMS "mechanisms"
@@ -74,12 +204,24 @@ class CSASLMod : public CModule {
             "RequireAuth",
             static_cast<CModCommand::ModCmdFunc>(&CSASLMod::RequireAuthCommand),
             "[yes|no]", "Don't connect unless SASL authentication succeeds");
+#ifdef HAVE_LIBSSL
+        AddCommand(
+            "GenerateKey",
+            static_cast<CModCommand::ModCmdFunc>(&CSASLMod::GenerateKey),
+            "", "Generate a key to use for ECDSA-NIST256P-CHALLENGE authentication");
+        AddCommand(
+            "ShowKey",
+            static_cast<CModCommand::ModCmdFunc>(&CSASLMod::ShowKey),
+            "",
+            "Prints base64 encoded key for ECDSA-NIST256P-CHALLENGE authentication");
+#endif
         AddCommand("Verbose", "yes|no", "Set verbosity level, useful to debug",
                    [&](const CString& sLine) {
             m_bVerbose = sLine.Token(1, true).ToBool();
             PutModule("Verbose: " + CString(m_bVerbose));
         });
 
+        m_bStartedECDSA = false;
         m_bAuthenticated = false;
     }
 
@@ -178,12 +320,56 @@ class CSASLMod : public CModule {
         }
     }
 
+#ifdef HAVE_LIBSSL
+    void GenerateKey(const CString& sLine) {
+        ECDACommon ec;
+        CString key;
+
+        ec.GenerateKey();
+        ec.SaveKey(GetSavePath());
+        key = ec.GetPubBase64();
+        PutModule("New public key " + key + " generated");
+    }
+
+    void ShowKey(const CString& sLine) {
+        ECDACommon ec;
+
+        if (!ec.LoadKey(GetSavePath()))
+            PutModule("No valid public key found");
+        else
+            PutModule("Public key: " + ec.GetPubBase64());
+    }
+
+    void AuthenticateECDSA(const CString& sLine) {
+        ECDACommon ec;
+
+        /* First call we just send username */
+        if (!m_bStartedECDSA) {
+            PutIRC("AUTHENTICATE " + GetNV("username").Base64Encode_n());
+            m_bStartedECDSA = true;
+            return;
+        }
+        m_bStartedECDSA = false;
+
+        /* Load key then sign and send */
+        if (!ec.LoadKey(GetSavePath())) {
+            PutModule("No valid public key found");
+            return;
+        }
+        PutIRC("AUTHENTICATE " + ec.Sign(sLine));
+    }
+#endif
+
     void Authenticate(const CString& sLine) {
         if (m_Mechanisms.GetCurrent().Equals("PLAIN") && sLine.Equals("+")) {
             CString sAuthLine = GetNV("username") + '\0' + GetNV("username") +
                                 '\0' + GetNV("password");
             sAuthLine.Base64Encode();
             PutIRC("AUTHENTICATE " + sAuthLine);
+#ifdef HAVE_LIBSSL
+        } else if (m_Mechanisms.GetCurrent().Equals("ECDSA-NIST256P-CHALLENGE")) {
+            AuthenticateECDSA(sLine);
+#ifdef
         } else {
             /* Send blank authenticate for other mechanisms (like EXTERNAL). */
             PutIRC("AUTHENTICATE +");
@@ -268,12 +454,18 @@ class CSASLMod : public CModule {
         CheckRequireAuth();
     }
 
-    void OnIRCDisconnected() override { m_bAuthenticated = false; }
+    void OnIRCDisconnected() override {
+        m_bAuthenticated = false;
+        m_bStartedECDSA = false;
+    }
 
     CString GetWebMenuTitle() override { return "SASL"; }
 
     bool OnWebRequest(CWebSock& WebSock, const CString& sPageName,
                       CTemplate& Tmpl) override {
+#ifdef HAVE_LIBSSL
+        ECDACommon ec;
+#endif
         if (sPageName != "index") {
             // only accept requests to index
             return false;
@@ -287,12 +479,23 @@ class CSASLMod : public CModule {
             }
             SetNV(NV_REQUIRE_AUTH, WebSock.GetParam("require_auth"));
             SetNV(NV_MECHANISMS, WebSock.GetParam("mechanisms"));
+#ifdef HAVE_LIBSSL
+            if(WebSock.HasParam("keygen")) {
+                ec.GenerateKey();
+                ec.SaveKey(GetSavePath());
+            }
+#endif
         }
 
         Tmpl["Username"] = GetNV("username");
         Tmpl["Password"] = GetNV("password");
         Tmpl["RequireAuth"] = GetNV(NV_REQUIRE_AUTH);
         Tmpl["Mechanisms"] = GetMechanismsString();
+#ifdef HAVE_LIBSSL
+        Tmpl["HaveLibSSL"] = "true";
+        if (ec.LoadKey(GetSavePath()))
+            Tmpl["PublicKey"] = ec.GetPubBase64();
+#endif
 
         for (const auto& it : SupportedMechanisms) {
             CTemplate& Row = Tmpl.AddRow("MechanismLoop");
@@ -307,6 +510,7 @@ class CSASLMod : public CModule {
   private:
     Mechanisms m_Mechanisms;
     bool m_bAuthenticated;
+    bool m_bStartedECDSA;
     bool m_bVerbose = false;
 };
 
